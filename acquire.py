2403734c51b48349117b986fba6850a0f5830a05
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-04-05 14:06:34 -0500

    Acuire iris,zillow, and titanic SQL  database

--- acquire.py
+++ acquire.py
@@ -1,36 +1,60 @@
 import pandas as pd
-import numpy as np
 import os
 
 from env import host, user, password
+#-----------------------------------------------------------------------------------------------------------------------
+# Make a function named get_titanic_data that returns the titanic data from the codeup data science database as a pandas data frame.
 
-def get_zillow_data(use_cache=True):
-    filename = "zillow.csv"
+def get_titanic_data(use_cache=True):
+    filename = "titanic.csv"
     if os.path.isfile(filename) and use_cache:
         print("Let me get that for you...")
-        # if Unnamed: 0 column exists, drop it
-        df = pd.read_csv(filename)
-        if 'Unnamed: 0' in df.columns:
-            df = df.drop(columns='Unnamed: 0')
-        else:
-            pass
-        return df
+        return pd.read_csv(filename)
     print("Sorry, nothing on file, let me create one for you...")
-    data = 'zillow'
+    data = 'titanic_db'
+    url = f'mysql+pymysql://{user}:{password}@{host}/{data}'
+    titanic_data = pd.read_sql('SELECT * FROM passengers', url)
+    titanic_data.to_csv(filename)
+    return titanic_data
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Make a function named get_iris_data that returns the data from the iris_db on the codeup data science database as a pandas dataframe.
+
+def get_iris_data(use_cache=True):
+    filename = "iris.csv"
+    if os.path.isfile(filename) and use_cache:
+        print("Let me get that for you...")
+        return pd.read_csv(filename)
+    print("Sorry, nothing on file, let me create one for you...")
+    data = 'iris_db'
+    url = f'mysql+pymysql://{user}:{password}@{host}/{data}'
+    iris_data = pd.read_sql('SELECT * FROM measurements LEFT JOIN species USING(species_id)', url)
+    iris_data.to_csv(filename)
+    return iris_data
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Make a function named get_telco_data that returns the data from the telco_churn database in SQL. 
+# In your SQL, be sure to join all 4 tables together, so that the resulting dataframe contains all the contract, payment, and internet service options.
+
+def get_telco_data(use_cache=True):
+    filename = "telco.csv"
+    if os.path.isfile(filename) and use_cache:
+        print("Let me get that for you...")
+        return pd.read_csv(filename)
+    print("Sorry, nothing on file, let me create one for you...")
+    data = 'telco_churn'
     url = f'mysql+pymysql://{user}:{password}@{host}/{data}'
     query = '''
-            SELECT *
-            FROM properties_2017 
-            LEFT JOIN propertylandusetype USING (propertylandusetypeid)
-            JOIN predictions_2017 USING (parcelid)
-            LEFT JOIN airconditioningtype USING (airconditioningtypeid)
-            LEFT JOIN architecturalstyletype USING (architecturalstyletypeid)
-            LEFT JOIN buildingclasstype USING (buildingclasstypeid)
-            LEFT JOIN heatingorsystemtype USING (heatingorsystemtypeid)
-            LEFT JOIN typeconstructiontype USING (typeconstructiontypeid)
-            LEFT JOIN storytype USING (storytypeid)
-            JOIN unique_properties USING (parcelid)
-            WHERE transactiondate >= '2017-%%-%%';'''
-    zillow17_data = pd.read_sql(query, url)
-    zillow17_data.to_csv(filename)
-    return zillow17_data
+    SELECT * FROM customers 
+    JOIN contract_types USING (contract_type_id) 
+    JOIN payment_types USING (payment_type_id) 
+    JOIN internet_service_types USING (internet_service_type_id)
+    '''
+    telco_data = pd.read_sql(query, url)
+    # drop columns that are not needed
+    telco_data = telco_data.drop(['customer_id', 'contract_type_id', 'payment_type_id', 'internet_service_type_id'], axis=1)
+    if 'Unnamed: 0' in telco_data.columns:
+        telco_data = telco_data.drop(['Unnamed: 0'], axis=1)
+    telco_data.to_csv(filename)
+    return telco_data
+
